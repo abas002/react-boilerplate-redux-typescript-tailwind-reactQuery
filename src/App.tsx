dfb02dfe86b6056a939e8a598bee8727d2469e81
a.bakhshi <a.bakhshi@hoshmand.local>
2022-05-28 17:11:53 +0430

    add toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import './App.css';
+import { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Outlet />
+      <ToastContainer
+        position={`bottom-center`}
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={true}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={`colored`}
+      />
+    </>
   );
 }
 
 export default App;
+
